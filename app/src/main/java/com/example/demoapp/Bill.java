0b0e9f05ce232d4b336064afbf3365abff0804d3
HaryoHH <conghai20@gmail.com>
2020-06-21 08:46:47 +0700

    change some method in bill and billManager

--- app/src/main/java/com/example/demoapp/Bill.java
+++ app/src/main/java/com/example/demoapp/Bill.java
@@ -1,14 +1,17 @@
 package com.example.demoapp;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Bill {
-    List<Food> foodList;
+    List<Item> itemList;
     private String totalPrice;
     private String time;
 
-    public void setFoodList(List<Food> foodList) {
-        this.foodList = foodList;
+    public Bill(){
+        itemList = new ArrayList<Item>();
+        totalPrice = "0";
+        time = "";
     }
 
     public void setTime(String time) {
@@ -24,13 +27,20 @@ public class Bill {
     }
 
     public String getTotalPrice() {
+        calcTotalPrice();
         return totalPrice;
     }
 
-    public List<Food> getFoodList() {
-        return foodList;
+    private void calcTotalPrice(){
+        int sum = 0;
+        for(int i = 0; i < itemList.size(); i++){
+            int itemPrice = Integer.valueOf(itemList.get(i).getPrice()) * Integer.valueOf(itemList.get(i).getQuantity());
+            sum += itemPrice;
+        }
+        totalPrice = String.valueOf(sum);
     }
 
-    private void calcTotalPrice(){
+    public void addNewItem(Item item){
+        itemList.add(item);
     }
 }
--- app/src/main/java/com/example/demoapp/BillManager.java
+++ app/src/main/java/com/example/demoapp/BillManager.java
@@ -4,12 +4,9 @@ import java.util.List;
 
 public class BillManager {
     private Bill bill = new Bill();
-    private List<Item> itemList;
 
     public Bill getBill() {
         return bill;
     }
-    public List<Item> getItemList() {
-        return itemList;
-    }
+
 }
