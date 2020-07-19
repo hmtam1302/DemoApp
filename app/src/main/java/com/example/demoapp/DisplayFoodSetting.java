5bad5d29d28076f29b593055cf6ba1314fb3aebe
Haryo <conghai20@gmail.com>
2020-07-20 05:40:37 +0700

    Revert "Revert "fix bugs in finding bill list""
    This reverts commit f0a3d94d145f789a02fc77a8dd3a5308fa1a5e12.

---
+++ app/src/main/java/com/example/demoapp/DisplayFoodSetting.java
@@ -0,0 +1,13 @@
+package com.example.demoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayFoodSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
